488ddd49304c0d4ac17d29acc0e8f1d97ec8fe9a
ZWMiller <zaglamir@gmail.com>
2016-10-14 13:53:16 -0600

    Update plotting to use colors in wordHighlight, add chapter breaks to html

--- toaesWebscrape/wordHighlighter.py
+++ toaesWebscrape/wordHighlighter.py
@@ -1,24 +1,17 @@
-from collections import Counter
 from collections import defaultdict
 from bs4 import BeautifulSoup
+import string
 import requests
 import operator
-from pprint import pprint
+import numpy as np
+import matplotlib.pyplot as plt
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-import numpy as np
-import matplotlib.pyplot as plt
 
 def clean_word_for_count(w): 
-  w.replace(',','')
-  w.replace('.','')
-  w.replace('\'','')
-  return w.lower()
-
-def print_counter_words(counter, num):
-  for w, c in counter.most_common(int(num)):
-    print w, " ", str(c)
+  wr = "".join(l for l in w if l not in string.punctuation)
+  return wr.lower()
 
 def filter_common_words(w):
   common_words = ['a', 'the', 'an', 'to', 'i', 'and', 'for', 'of', 'is', 'in', 'that', 'it', 'on', 'you',
@@ -32,8 +25,18 @@ def get_max_counts_in_dictionary(dictionary):
   for w in sorted(dictionary, key=dictionary.get, reverse=True):
     return dictionary[w]
 
+def print_dictionary(dictionary):
+  for w in sorted(dictionary, key=dictionary.get, reverse=True):
+    print w + " " + str(dictionary[w])
+
 def get_word_count_ratio(word, dictionary):
   return float(float(dictionary[word])/float(get_max_counts_in_dictionary(dictionary)))
+
+def is_title_text(text):
+  if text == "Thoughts of An Eaten Sun > v4":
+    return True
+  else:
+    return False
 
 def get_word_color(word,dictionary):
   r = get_word_count_ratio(word,dictionary)
@@ -41,7 +44,7 @@ def get_word_color(word,dictionary):
  
 def get_color(r):
   if r > 0.9:
-    return '#ff0000'
+    return 'ff0000'
   elif r > 0.8:
     return 'ff6600'
   elif r > 0.7:
@@ -66,9 +69,11 @@ def make_word_count_plot(dictionary):
   limit=100
   x = list()
   y = list()
+  colors = list()
   for w in sorted(dictionary, key=dictionary.get, reverse=True):
     x.append(w)
     y.append(dictionary[w])
+    colors.append('#'+get_word_color(clean_word_for_count(w),dictionary))
     index+=1
     if index >= limit: 
       break
@@ -76,12 +81,17 @@ def make_word_count_plot(dictionary):
   bar_width = 0.35
   opacity = 0.4
   index2 = np.arange(limit)
-  rects1 = plt.bar(index2, y, bar_width, alpha=opacity, color='b')
+  rects1 = plt.bar(index2, y, bar_width, alpha=opacity, color=colors)
   plt.xlabel('Words')
   plt.ylabel('Counts')
   plt.title('{} Most Common Words in Text'.format(str(limit)))
-  plt.xticks(index2 + bar_width, x, rotation=90, fontsize=12)
-  plt.tight_layout()
+  plt.xticks(index2 + bar_width, x, rotation=90, fontsize=11)
+  for ii in reversed(range(0,10)):
+    il = float(ii)/10.
+    ypos = 0.85 - float(ii)/30.
+    ii = ii*10
+    colors = '#'+get_color(0.05+il)
+    ax.text(0.65,ypos,'Top {}% of Words'.format(ii),color=colors,transform = ax.transAxes)
   plt.show()
 
 def make_html_output(page_name,unfiltered_wordlist,dictionary):
@@ -106,7 +116,6 @@ def make_html_output(page_name,unfiltered_wordlist,dictionary):
     file.write('\n{}\n{}\n{}'.format(line1,line2,line3))
 
 
-
 ###################
 ## Main Program
 ###################
@@ -119,17 +128,22 @@ if __name__ == "__main__":
     url = "http://toaes.kyletolle.com/v4/ch" + str(i) + "/"
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html5lib')
-    text = soup.get_text()
-    #print text
-    for word in text.strip().split():
-      unfiltered_wordlist.append(word)
-      if filter_common_words(word.lower()): 
+    for x in soup.findAll("p"):
+      #print x.text #DEBUG
+      if is_title_text(x.text):
         continue
-      wordlist.append(clean_word_for_count(word))
+      for word in x.text.strip().split():
+        unfiltered_wordlist.append(word)
+        if filter_common_words(word.lower()): 
+          continue
+        wordlist.append(clean_word_for_count(word))
+    unfiltered_wordlist.append('<br><br>')
   
   dictionary = defaultdict(int)
   for w in wordlist:
     dictionary[w]+=1
+
+  #print_dictionary(dictionary) #DEBUG
   
   #make plot from dictionary
   make_word_count_plot(dictionary)
