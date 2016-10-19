237e08489c6fd3ef494b82005787fa1ee4b15cc4
ZWMiller <zaglamir@gmail.com>
2016-10-19 08:24:57 -0500

    Add advanced stat calculation function

--- MLBAnalysis/MLBAnalysis.py
+++ MLBAnalysis/MLBAnalysis.py
@@ -42,36 +42,31 @@ def get_mlb_data_from_csv():
       sh = int(sh)
       sf = int(sf)
       gidp = int(gidp)
-
       if playerID not in stat_dictionary:
         stat_dictionary[playerID]={}
       stat_dictionary[playerID][year] = {
-          "stint": stint,
-          "teamID": teamID,
-          "lgID": lgID,
-          "G": g,
-          "AB": ab,
-          "R": r,
-          "H": h,
-          "X2B": x2b,
-          "X3B": x3b,
-          "HR": hr,
-          "RBI": rbi,
-          "SB": sb,
-          "CS": cs,
-          "BB": bb,
-          "SO": so,
-          "IBB": ibb,
-          "HBP": hbp,
-          "SH": sh,
-          "SF": sf,
-          "GIDP": gidp,
-          "PA": ab+bb+hbp,
+          "stint": stint, "teamID": teamID, "lgID": lgID, "G": g, "AB": ab, "R": r, "H": h, "X2B": x2b, "X3B": x3b, "HR": hr, "RBI": rbi, 
+          "SB": sb, "CS": cs, "BB": bb, "SO": so, "IBB": ibb, "HBP": hbp, "SH": sh, "SF": sf, "GIDP": gidp,
       }
   return stat_dictionary
 
+def calculate_processed_stats(d):
+  for p in d:
+    for y in d[p]:
+      if d[p][y]["AB"] == 0:
+        avg = 0
+        iso = 0
+        pa = d[p][y]["AB"]+d[p][y]["BB"]+d[p][y]["HBP"]
+      else:
+        avg = float(d[p][y]["H"])/float(d[p][y]["AB"])
+        pa = d[p][y]["AB"]+d[p][y]["BB"]+d[p][y]["HBP"]
+        iso = (float(d[p][y]["X2B"])+2.*float(d[p][y]["X3B"])+3.*float(d[p][y]["HR"]))/float(d[p][y]["AB"])
+      d[p][y].update( {"AVG": avg, "PA": pa, "ISO": iso} )
+      
+
 if __name__ == "__main__":
   stat_dictionary = get_mlb_data_from_csv()
+  calculate_processed_stats(stat_dictionary)
   for player in stat_dictionary:
     for year in stat_dictionary[player]:
       if year == 2009 and stat_dictionary[player][year]["teamID"] == "cin":
