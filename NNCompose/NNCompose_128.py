0dcdcde5e766f19c3bafda5d0290a95d869791a5
ZWMiller <zaglamir@gmail.com>
2017-01-25 10:07:32 -0600

    Attempting to add scikit cross validation via pipeline (not a working commit)

--- NNCompose/NNCompose_128.py
+++ NNCompose/NNCompose_128.py
@@ -5,10 +5,17 @@ import numpy as np
 import pandas as pd
 import random
 import csv
+import time
 # Create first network with Keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.utils import np_utils
+from keras.wrappers.scikit_learn import KerasRegressor
+#sciKit for pipeline and CV
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 class midiFile(object):
     def __init__(self,fname):
@@ -49,7 +56,8 @@ class midiFile(object):
 
     def convertNotesToName(self):
         notename = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
-        self.df['noteName'] = self.df['note'].apply(lambda x: int(x % 12))
+        #self.df['noteName'] = self.df['note'].apply(lambda x: int(x % 12))
+        self.df['noteName'] = self.df['note'] #if trying to include octaves)
 
     def convertMidiToMp3(self):
         basename = os.path.splitext(self.oname)
@@ -57,6 +65,9 @@ class midiFile(object):
         outname = "mp3Out/" + str(basename[0]) + ".mp3"
         cmd = "timidity -Ow -o - " + inname + "| lame - " + outname
         mp3Created = subprocess.Popen(cmd, shell=True)
+        time.sleep(5)
+        print "Created " + outname
+        
 
     def makeMidiOut(self,notes):
         basename = os.path.splitext(self.oname)
@@ -96,25 +107,22 @@ class network(object):
     def getModel(self):
         # create model
         model = Sequential()
-        model.add(Dense(12, input_dim=notesToUse, init='uniform', activation='relu'))
-        model.add(Dense(10, init='uniform', activation='relu'))
-        model.add(Activation('relu'))
-        model.add(Dense(8, init='uniform', activation='relu'))
-        model.add(Dense(12, init='uniform', activation='softmax'))
+        model.add(Dense(256, input_dim=notesToUse, init='uniform', activation='relu'))
+        model.add(Dense(200, init='uniform', activation='relu'))
+        model.add(Dense(150, init='uniform', activation='relu'))
+        model.add(Dense(128, init='uniform', activation='softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         return model
-
-    def compile(self):
-        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 if __name__ == "__main__":
     #mf = midiFile("ForestMaze.mid")
     mf = midiFile("ForestMaze.mid")
-    mf.convertNotesToName()
-    notes = mf.df['noteName'].values
+   # mf.convertNotesToName()
+    notes = mf.df['note'].values
     
     # split into input (X) and output (Y) variables
     i=0
-    notesToUse = 16;
+    notesToUse = 24;
     X = np.empty([int(mf.df.shape[0]),notesToUse],dtype=int)
     Yx = np.empty([int(mf.df.shape[0]),1],dtype=int)
     while i+notesToUse+1 <= notes.size:
@@ -123,18 +131,25 @@ if __name__ == "__main__":
         i+=1
 
     # one-hot encode output to vector or booleans
-    Y = np_utils.to_categorical(Yx,nb_classes=12)
+    Y = np_utils.to_categorical(Yx,nb_classes=128)
     
     # fix random seed for reproducibility
     seed = 7
     np.random.seed(seed)
     # Get and Compile model
     NN = network()
-    NN.compile()
+    estimators = []
+    estimators.append(('standardize', StandardScaler()))
+    estimators.append(('mlp', KerasRegressor(build_fn=NN.model, nb_epoch=50, batch_size=5, verbose=2)))
+    pipeline = Pipeline(estimators)
+    kfold = KFold(n_splits=10, random_state=seed)
+    results = cross_val_score(pipeline, X, Y, cv=kfold)
+    print("Standardized: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+    
     # Fit the model
-    NN.model.fit(X, Y, nb_epoch=150, batch_size=10,  verbose=2)
+    #NN.model.fit(X, Y, nb_epoch=50, batch_size=10,  verbose=2)
     # calculate predictions
-    predictions = NN.model.predict(X)
+    predictions = pipeline.predict(X)
     compare = zip(Y,predictions)
     #for x,y in compare[0:10]:
     #    print x
@@ -147,30 +162,40 @@ if __name__ == "__main__":
     currentNotes = X[0:1].tolist()
     #print currentNotes
     notes = []
+    prevNote = 0
     for j in range(0,notesToGen):
         ind = 0
         val = 0
        # newNote = NN.model.predict(X[j:j+1]).tolist()
-        print np.asarray([currentNotes[0][len(currentNotes[0])-notesToUse:len(currentNotes[0])]])
-        newNote = NN.model.predict(np.asarray([currentNotes[0][len(currentNotes[0])-notesToUse:len(currentNotes[0])]])).tolist()
-        print newNote
+       # newNote = NN.model.predict(np.asarray([currentNotes[0][len(currentNotes[0])-notesToUse:len(currentNotes[0])]])).tolist()
+        newNote = pipeline.predict(np.asarray([currentNotes[0][len(currentNotes[0])-notesToUse:len(currentNotes[0])]])).tolist()
         for i,j in enumerate(newNote[0]):
             if j>val:
                 ind = i
                 val = j
+    
+        if ind == prevNote and random.random()>0.5:
+            val = 0
+            ind = 0
+            for i,j in enumerate(newNote[0]):
+                if j>val and i != prevNote:
+                    ind = i
+                    val = j
+        prevNote = ind
+
         #print ind, newNote[0][ind]
         currentNotes[0].append(ind)
         currentNotes[0].pop(0)
         #print currentNotes
         #newNote = random.randint(0,11)
-        notes.append([2,currentTime, "Note_on_c", 2, 60+int(ind), 95,False])
+        notes.append([2,currentTime, "Note_on_c", 2, int(ind), 95,False])
         #notes.append([2,currentTime, "Note_on_c", 2, 60+int(newNote), 95, False])
         beatLength = 3
         while beatLength == 3:
             beatLength = random.randint(1,4)
         dt = int(quarterNote/beatLength)
         currentTime = currentTime+dt
-        notes.append([2,currentTime, "Note_off_c", 2, 60+int(ind), 95, False])
+        notes.append([2,currentTime, "Note_off_c", 2, int(ind), 95, False])
         #notes.append([2,currentTime, "Note_off_c", 2, 60+int(newNote), 95, False])
     notes.append([2, currentTime, "End_track"])
     mf.makeMidiOut(pd.DataFrame(notes))
